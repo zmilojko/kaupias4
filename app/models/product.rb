64b3fb850db8f3a041b7d4b9a10bda008f715215
oscarsan <oscarsanhueza@gmail.com>
2012-01-12 21:03:37 +0200

    Revert "Changes made in Z home to make it work with right database and take out sqllite gem"
    This reverts commit eee4b1b1b76015c6706f2735ac03124149133060.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ActiveRecord::Base
   has_many :descriptions, :class_name => 'ProductDescription', :foreign_key => :products_id  
 
   def name
-    descriptions.where(:language_id => 1)[0].products_name 
+    descriptions.where(:language_id => 2)[0].products_name 
   end
 end
